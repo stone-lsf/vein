e30ed1be60afe4f25b8cad022cd1101109b8f71b
stone.lsf <stone.lsf@alibaba-inc.com>
2017-08-30 17:47:39 +0800

    init

---
+++ charge/src/main/java/com/sm/finance/charge/server/Test.java
@@ -0,0 +1,8 @@
+package com.sm.finance.charge.server;
+
+/**
+ * @author shifeng.luo
+ * @version created on 2017/8/30 下午5:46
+ */
+public class Test {
+}
---
+++ notify/src/main/java/com/sm/finance/charge/notify/Test.java
@@ -0,0 +1,8 @@
+package com.sm.finance.charge.notify;
+
+/**
+ * @author shifeng.luo
+ * @version created on 2017/8/30 下午5:46
+ */
+public class Test {
+}
